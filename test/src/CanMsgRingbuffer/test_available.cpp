17656288a5acc90239e26a3558d44118d023908c
Jeremy Boynes <jeremy@boynes.com>
2023-09-07 13:48:45 +0100

    Fix deprecated volatile access in CanMsgRingbuffer + testcase

---
+++ test/src/CanMsgRingbuffer/test_available.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2020 Arduino.  All rights reserved.
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <catch.hpp>
+
+#include <CanMsgRingbuffer.h>
+
+/**************************************************************************************
+ * TEST CODE
+ **************************************************************************************/
+
+TEST_CASE ("'available' should return 0 for empty CanMsg ring buffer", "[CanMsgRingbuffer-available-01]")
+{
+  arduino::CanMsgRingbuffer ringbuffer;
+  REQUIRE(ringbuffer.available() == 0);
+}
+
+TEST_CASE ("'available' should return number of elements in CanMsg ringbuffer", "[CanMsgRingbuffer-available-02]")
+{
+  arduino::CanMsgRingbuffer ringbuffer;
+  arduino::CanMsg msg;
+  ringbuffer.enqueue(msg);
+  REQUIRE(ringbuffer.available() == 1);
+  ringbuffer.enqueue(msg);
+  REQUIRE(ringbuffer.available() == 2);
+  ringbuffer.dequeue();
+  REQUIRE(ringbuffer.available() == 1);
+}
